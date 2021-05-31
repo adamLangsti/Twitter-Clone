34fc083a2920e739a23ea8362a4fb8921d632731
Adam Lang <adam.lang-1@hotmail.com>
2021-05-31 19:17:09 +0200

    feat: widgest now displaying twitter timeline

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from 'react-twitter-embed';
 
 const Widgets = () => {
     return (
         <div className='widgets'>
-            <h2>Widgets</h2>
+            <div className='widgets-input'>
+                <SearchIcon className='widgets-searchIcon' />
+                <input type='text' placeholder='Search Twitter' />
+            </div>
+            <div className='widgets-widgetContainer'>
+                <h2>What's happening</h2>
+                <TwitterTweetEmbed tweetId={'858551177860055040'} />
+                <TwitterTimelineEmbed
+                    sourceType='profile'
+                    screenName='adamlang121'
+                    options={{ height: 400 }}
+                />
+                <TwitterShareButton
+                    url={'https://www.facebook.com/adam.lang.758'}
+                    options={{ text: 'reactjs' }}
+                />
+            </div>
         </div>
     );
 };
